8b93e23a42667f465c13f202c37f7be6c92a3bd1
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2024-04-10 18:12:39 -0400

    update final figures

--- plot_allpatients.m
+++ plot_allpatients.m
@@ -79,15 +79,16 @@ AddLetters2Plots(figure(1),{'A','B'},'HShift', -0.06, 'VShift', -0.06, ...
 
 %% Plot T cells
 % Make plots for NR, CR, PR
-ymin = 1e-10;
+ymin = 0;
+ymax = 60;
 figure(2);
 clf;
 subplot(2,2,1)
 xlabel('time (day)')
 ylabel('T_m (cells/\muL)')
 xlim([0,365])
-ylim([ymin,1e2])
-set(gca,'fontsize',fgca, 'YScale','log')
+ylim([ymin,ymax])
+set(gca,'fontsize',fgca)
 grid on
 hold on
 
@@ -95,8 +96,8 @@ subplot(2,2,2)
 xlabel('time (day)')
 ylabel('T_{e1} (cells/\muL)')
 xlim([0,365])
-ylim([ymin,1e2])
-set(gca,'fontsize',fgca, 'YScale','log')
+ylim([ymin,ymax])
+set(gca,'fontsize',fgca)
 grid on
 hold on
 
@@ -104,8 +105,8 @@ subplot(2,2,3)
 xlabel('time (day)')
 ylabel('T_{e2} (cells/\muL)')
 xlim([0,365])
-ylim([ymin,1e2])
-set(gca,'fontsize',fgca, 'YScale','log')
+ylim([ymin,ymax])
+set(gca,'fontsize',fgca)
 grid on
 hold on
 
@@ -113,8 +114,139 @@ subplot(2,2,4)
 xlabel('time (day)')
 ylabel('T_{x} (cells/\muL)')
 xlim([0,365])
-ylim([ymin,1e2])
-set(gca,'fontsize',fgca, 'YScale','log')
+ylim([ymin,ymax])
+set(gca,'fontsize',fgca)
+grid on
+hold on
+
+sf = 1/2e6; % scaling factor
+
+% Set up vectors for getting output at specific time (used for next figure)
+t_stop = 365;
+t_output = [0:0.05:10 11:1:t_stop]; 
+
+Tm_sim = zeros(length(t_output), 12,3); % 1 -- NR, 2 -- PR, 3 -- CR
+Te1_sim = zeros(length(t_output),12,3);
+Te2_sim = zeros(length(t_output),12,3);
+Tx_sim = zeros(length(t_output),12,3);
+
+for ii = 1:12
+    cNR = cmapNR(2+ii,:);
+    cCR = cmapCR(2+ii,:);
+    cPR = cmapPR(2+ii,:);
+
+    fnamebase = strcat('./sims_patients/',...
+                    date_str,...
+                    '_KirouacPatients',...
+                    '_patID-', num2str(ii),...
+                    '_dose-', num2str(cell_dose));
+    fnameCR = strcat(fnamebase, '_CR.mat');
+    fnameNR = strcat(fnamebase,'_NR.mat');
+    fnamePR = strcat(fnamebase,'_PR.mat');
+
+    datCR = load(fnameCR);
+    datNR = load(fnameNR);
+    datPR = load(fnamePR);
+
+    % Tm
+    subplot(2,2,1)
+    plot(datNR.tNR, datNR.yNR(:,1)*sf, 'linewidth',lw, 'color', cNR)
+    plot(datCR.tCR, datCR.yCR(:,1)*sf, 'linewidth',lw, 'color', cCR)
+    plot(datPR.tPR, datPR.yPR(:,1)*sf, 'linewidth',lw, 'color', cPR)
+
+    % Interpolate specific points
+    Tm_sim(:,ii,1) = interp1(datNR.tNR, datNR.yNR(:,1), t_output);
+    Tm_sim(:,ii,2) = interp1(datPR.tPR, datPR.yPR(:,1), t_output);
+    Tm_sim(:,ii,3) = interp1(datCR.tCR, datCR.yCR(:,1), t_output);
+
+
+    % Te1
+    subplot(2,2,2)
+    plot(datNR.tNR, datNR.yNR(:,2)*sf, 'linewidth',lw, 'color', cNR)
+    plot(datCR.tCR, datCR.yCR(:,2)*sf, 'linewidth',lw, 'color', cCR)
+    plot(datPR.tPR, datPR.yPR(:,2)*sf, 'linewidth',lw, 'color', cPR)
+
+    % Interpolate specific points
+    Te1_sim(:,ii,1) = interp1(datNR.tNR, datNR.yNR(:,2), t_output);
+    Te1_sim(:,ii,2) = interp1(datPR.tPR, datPR.yPR(:,2), t_output);
+    Te1_sim(:,ii,3) = interp1(datCR.tCR, datCR.yCR(:,2), t_output);
+
+    % Te2
+    subplot(2,2,3)
+    plot(datNR.tNR, datNR.yNR(:,3)*sf, 'linewidth',lw, 'color', cNR)
+    plot(datCR.tCR, datCR.yCR(:,3)*sf, 'linewidth',lw, 'color', cCR)
+    plot(datPR.tPR, datPR.yPR(:,3)*sf, 'linewidth',lw, 'color', cPR)
+
+    % Interpolate specific points
+    Te2_sim(:,ii,1) = interp1(datNR.tNR, datNR.yNR(:,3), t_output);
+    Te2_sim(:,ii,2) = interp1(datPR.tPR, datPR.yPR(:,3), t_output);
+    Te2_sim(:,ii,3) = interp1(datCR.tCR, datCR.yCR(:,3), t_output);
+
+
+    % Tx
+    subplot(2,2,4)
+    plot(datNR.tNR, datNR.yNR(:,4)*sf, 'linewidth',lw, 'color', cNR)
+    plot(datCR.tCR, datCR.yCR(:,4)*sf, 'linewidth',lw, 'color', cCR)
+    plot(datPR.tPR, datPR.yPR(:,4)*sf, 'linewidth',lw, 'color', cPR)
+
+    % Interpolate specific points
+    Tx_sim(:,ii,1) = interp1(datNR.tNR, datNR.yNR(:,4), t_output);
+    Tx_sim(:,ii,2) = interp1(datPR.tPR, datPR.yPR(:,4), t_output);
+    Tx_sim(:,ii,3) = interp1(datCR.tCR, datCR.yCR(:,4), t_output);
+end
+subplot(2,2,1)
+legend('NR', 'CR', 'PR','location','best','fontsize',fleg)
+subplot(2,2,2)
+legend('NR', 'CR', 'PR','location','best','fontsize',fleg)
+subplot(2,2,3)
+legend('NR', 'CR', 'PR','location','best','fontsize',fleg)
+subplot(2,2,4)
+legend('NR', 'CR', 'PR','location','best','fontsize',fleg)
+
+AddLetters2Plots(figure(2),{'A','B','C','D'},'HShift', -0.06, 'VShift', -0.06, ...
+                'fontsize', 22)
+
+
+%% Repeat of Fig 2 with smaller x scale
+figure(3)
+xmin = 0; xmax = 100;
+% Make plots for NR, CR, PR
+ymin = 0;
+ymax = 60;
+clf;
+subplot(2,2,1)
+xlabel('time (day)')
+ylabel('T_m (cells/\muL)')
+xlim([xmin,xmax])
+ylim([ymin,40])
+set(gca,'fontsize',fgca)
+grid on
+hold on
+
+subplot(2,2,2)
+xlabel('time (day)')
+ylabel('T_{e1} (cells/\muL)')
+xlim([xmin,xmax])
+ylim([ymin,ymax])
+set(gca,'fontsize',fgca)
+grid on
+hold on
+
+subplot(2,2,3)
+xlabel('time (day)')
+ylabel('T_{e2} (cells/\muL)')
+xlim([xmin,xmax])
+ylim([ymin,30])
+set(gca,'fontsize',fgca)
+grid on
+hold on
+
+subplot(2,2,4)
+xlabel('time (day)')
+ylabel('T_{x} (cells/\muL)')
+xlim([xmin,xmax])
+ylim([ymin,20])
+set(gca,'fontsize',fgca)
 grid on
 hold on
 
@@ -145,11 +277,14 @@ for ii = 1:12
     plot(datPR.tPR, datPR.yPR(:,1)*sf, 'linewidth',lw, 'color', cPR)
 
 
+
     % Te1
     subplot(2,2,2)
     plot(datNR.tNR, datNR.yNR(:,2)*sf, 'linewidth',lw, 'color', cNR)
     plot(datCR.tCR, datCR.yCR(:,2)*sf, 'linewidth',lw, 'color', cCR)
     plot(datPR.tPR, datPR.yPR(:,2)*sf, 'linewidth',lw, 'color', cPR)
+
+
 
     % Te2
     subplot(2,2,3)
@@ -158,11 +293,13 @@ for ii = 1:12
     plot(datPR.tPR, datPR.yPR(:,3)*sf, 'linewidth',lw, 'color', cPR)
 
 
+
     % Tx
     subplot(2,2,4)
     plot(datNR.tNR, datNR.yNR(:,4)*sf, 'linewidth',lw, 'color', cNR)
     plot(datCR.tCR, datCR.yCR(:,4)*sf, 'linewidth',lw, 'color', cCR)
     plot(datPR.tPR, datPR.yPR(:,4)*sf, 'linewidth',lw, 'color', cPR)
+
 end
 subplot(2,2,1)
 legend('NR', 'CR', 'PR','location','best','fontsize',fleg)
@@ -173,11 +310,8 @@ legend('NR', 'CR', 'PR','location','best','fontsize',fleg)
 subplot(2,2,4)
 legend('NR', 'CR', 'PR','location','best','fontsize',fleg)
 
-AddLetters2Plots(figure(2),{'A','B','C','D'},'HShift', -0.06, 'VShift', -0.06, ...
+AddLetters2Plots(figure(3),{'A','B','C','D'},'HShift', -0.06, 'VShift', -0.06, ...
                 'fontsize', 22)
 
 
-%% Plot the mean results instead.... with the confident intervals (IDEA)
-% This could give me an overview of the general trajectories for the 3
-% groups
 
